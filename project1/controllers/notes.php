698d9f354416f2396d08e9c059d457a09d5f095c
LuminousBlade <164029083+LuminousBlade@users.noreply.github.com>
2024-06-04 11:27:36 +0530

    feat: add new file for notes of user
    add all the notes users write from mariadb database to the web according to their id

--- project1/controllers/notes.php
+++ project1/controllers/notes.php
@@ -5,7 +5,7 @@ $db = new Database($config['database']);
 
 $heading = "Notes";
 
-$notes = [];
-dd($db);
+$id = $_GET['id'];
+$notes = $db->query('select * from notes where user_id = 3')->fetchAll();
 
 require "view/notesView.php";
