35245e0e69763fd003f4dd52d151874065927a6c
LuminousBlade <164029083+LuminousBlade@users.noreply.github.com>
2024-06-04 11:29:31 +0530

    feat: add visual data to notes file
    add the list visible and all the other css and html part visible in the notes file

--- project1/view/notesView.php
+++ project1/view/notesView.php
@@ -4,10 +4,13 @@
 
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-    <p> Welcome to notes section.  </p>
+    <?php foreach ($notes as $note) : ?>
+    <li> 
+    <a href="/note?id=<?= $note['id']?>" class="text-blue-500 hover:underline">
+        <?= $note["body"] ?> 
+    </li>
+    <?php endforeach; ?>
     </div>
   </main>
 
   <?php require('partials/footer.php') ?>
-
-
