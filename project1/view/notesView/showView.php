118adf5681a18e22d62a195cb7551a9e9875b905
LuminousBlade <164029083+LuminousBlade@users.noreply.github.com>
2024-06-07 13:47:37 +0530

    feat: add css to show file and safety protocol for any type of tag
    refer to indexView file for more info.

---
+++ project1/view/notesView/showView.php
@@ -0,0 +1,14 @@
+<?php require('view/partials/header.php') ?>
+<?php require('view/partials/nav.php') ?>
+<?php require('view/partials/banner.php') ?>
+
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <p class="mb-6">
+        <a href="/notes" class="text-blue-500 underline"> GO BACK </a> 
+      </p>
+        <p><?= htmlspecialchars($note["body"]) ?></p>
+    </div>
+  </main>
+
+  <?php require('view/partials/footer.php') ?>
